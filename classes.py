9fc6aba9ef2dc751719f54e99b64a85f2299914c
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-18 18:25:47 +0545

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Pascal:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point  = Pascal(10,20)
+print(point.x,point.y)
