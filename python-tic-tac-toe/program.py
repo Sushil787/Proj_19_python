27d4045851ff77000b3c413dfabe2a915bc6fc1a
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2020-10-24 17:42:59 +0545

    Update program.py
    updated now we can use empty box.

--- python-tic-tac-toe/program.py
+++ python-tic-tac-toe/program.py
@@ -1,4 +1,5 @@
-lists = [0,1,2,3,4,5,6,7,8,9]
+lists = [" "," "," "," "," "," "," "," "," "," "]
+#lists = [0,1,2,3,4,5,6,7,8,9] updated here now we can use empty box
 
 def board():
     print(f"{lists[1]}   |  {lists[2]}   |   {lists[3]}")
@@ -7,34 +8,43 @@ def board():
     print("----------------")
     print(f"{lists[7]}   |  {lists[8]}   |   {lists[9]}")
     print("\n")
+
+'''def board():
+    print("    |      |    ")
+    print("----------------")
+    print("    |      |    ")
+    print("----------------")
+    print("    |      |    ")
+    print("\n")'''
+
 def appends(userInput):
-    if(userInput in lists):
+    if(userInput not in lists):
         lists.pop(userInput)
         if(inputs == 1):
           lists.insert(userInput,"x")
         else:
             lists.insert(userInput,"O")
 def checkWin():
-    if(lists[1]==lists[2] and lists[2] == lists[3]):
+    if(lists[1]==lists[2] and lists[2] == lists[3] and lists[1]!= " "):
         return True
-    if(lists[4]==lists[5] and lists[5] == lists[6]):
+    if(lists[4]==lists[5] and lists[5] == lists[6] and lists[4]!= " "):
         return True
-    if(lists[7]==lists[8] and lists[8] == lists[9]):
+    if(lists[7]==lists[8] and lists[8] == lists[9] and lists[7]!= " "):
         return True
-    if(lists[1]==lists[4] and lists[4] == lists[7]):
+    if(lists[1]==lists[4] and lists[4] == lists[7] and lists[7]!= " "):
         return True
-    if(lists[2]==lists[5] and lists[5] == lists[8]):
+    if(lists[2]==lists[5] and lists[5] == lists[8]and lists[8]!= " "):
         return True
-    if(lists[3]==lists[6] and lists[6] == lists[9]):
+    if(lists[3]==lists[6] and lists[6] == lists[9] and lists[9]!= " "):
         return True
-    if(lists[1]==lists[5] and lists[5] == lists[9]):
+    if(lists[1]==lists[5] and lists[5] == lists[9] and lists[5]!= " "):
         return True
-    if(lists[3]==lists[5] and lists[5] == lists[7]):
+    if(lists[3]==lists[5] and lists[5] == lists[7] and lists[3]!= " "):
         return True
     else:
         return False
         
-print("Wlcome to Game")
+print("Welcome to TIC-TAC-TOE Game")
 player1 = input("input your name : ")
 player2 = input("input your name :")
 print(f"Players activated .{player1}  is player-1")
@@ -66,4 +76,4 @@ for i in range(1,10):
 
 else:
     print("game drawed ! noone won try next time")   
-        
+    
